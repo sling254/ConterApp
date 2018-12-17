75fa726f826bfb71698f9a0d4498b4f55a657625
Sling254 <mwanikidenis7@gmail.com>
2018-12-17 15:38:05 +0300

    first commit

---
+++ app/src/main/java/com/example/sling/counterapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.sling.counterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /**
+     * Show a toast
+     * @param view -- the view that is clicked
+     */
+    public void toastMe(View view){
+        // Toast myToast = Toast.makeText(this, message, duration);
+        Toast myToast = Toast.makeText(this, "Hello Toast!",
+                Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+    public void countMe (View view) {
+        // Get the text view.
+        TextView showCountTextView = (TextView)
+                findViewById(R.id.textView);
+
+        // Get the value of the text view.
+        String countString = showCountTextView.getText().toString();
+
+        // Convert value to a number and increment it.
+        Integer count = Integer.parseInt(countString);
+        count++;
+
+        // Display the new value in the text view.
+        showCountTextView.setText(count.toString());
+    }
+
+}
